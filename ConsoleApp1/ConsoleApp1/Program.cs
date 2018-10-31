ca4ba192683eb0cb101c9380ba3f57d8c62328f5
pnobrept <paulo.nobre@gmail.com>
2018-10-31 18:32:53 +0000

    Version 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            for (int l = 1; l <= 8; l++)
+            {
+                s = "";
+                for (int c = 1; c <= 8; c++)
+                {
+                    if ((c+l)%2==0) 
+                        s = s + "X";
+                    else
+                        s = s + "O";
+                }
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
